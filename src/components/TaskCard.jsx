ae91821b8450394e232eba25107ca7d15808431e
Héctor Meillon <134432174+hmeillon@users.noreply.github.com>
2024-02-21 16:26:55 -0600

    Create the new context file called TaskContext that will help us pass data directly to every children without prop drilling, also modify all de components so it can work together.

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,10 +1,15 @@
+import { useContext } from "react";
+import { TaskContext } from "../context/TaskContext";
+
 //This component will create different tasks everytime we add info into our inputs, the data will travel from App(), through TaskList to here, so now that data (tasks.js info) can be placed and then it will be exported and render the info as cards in our homepage. { data } will import the info from TaskList, also passing deleteTask function via TaskList.
-function TaskCard({ data, deleteTask }) {
+function TaskCard({ data }) {
+  const { deleteTask } = useContext(TaskContext);
+
   return (
     <div>
       <h2>{data.title}</h2>
       <p>{data.description}</p>
-      {/*This button will execute, once clicked the deleteTask function and pass as an argument the task id*/}
+      {/*This button will execute, once clicked the deleteTask function and pass as an argument the task*/}
       <button onClick={() => deleteTask(data.id)}>Delete</button>
     </div>
   );
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,8 +1,12 @@
 //Since this is one of the utilities of React, we are going to import a component inside this component, ins this case we are going to do that because we can REUSE the info inside that component, why? Because we can create different tasks with that.
 import TaskCard from "./TaskCard";
+import { useContext } from "react";
+import { TaskContext } from "../context/TaskContext";
 
 //This function, unlike TaskForm that is in charge of creating new tasks, it will render the in the webpage. Also passing the deleteTask function.
-function TaskList({ data, deleteTask }) {
+function TaskList() {
+  const { data } = useContext(TaskContext);
+
   //This will help us handle in case that we dont have any data in the "backend" (our tasks.js file).
   if (data.length === 0) {
     return <h1>No tasks yet.</h1>;
@@ -12,7 +16,7 @@ function TaskList({ data, deleteTask }) {
   return (
     <div>
       {data.map((data) => (
-        <TaskCard key={data.id} data={data} deleteTask={deleteTask} />
+        <TaskCard key={data.id} data={data} />
       ))}
     </div>
   );
